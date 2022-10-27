0742689ff392f4d8d95f4c86be628bb47c76f555
awmarshall64@gmail.com <awmarshall64@gmail.com>
2022-10-27 13:49:11 -0400

    oddsandevens deliverable

---
+++ Ex1/src/oddsandevens.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+/**
+ * @author erino
+ *
+ */
+import java.util.Scanner;
+
+
+	public class oddsandevens {
+
+	
+
+	     public static void main(String[] args) {
+	     String oddsandevens;
+	     	 	int number=0;
+	 	
+	 	System.out.println("Enter a number between 1 and 100:");
+	 	Scanner userEntry = new Scanner(System.in);
+		oddsandevens = userEntry.nextLine();
+
+		if(number % 2==0 && number >= 25); {
+			System.out.println(oddsandevens + " Even and less than 25");
+  	 	   }
+		else if (number % 2 ==0 && 25 < number >= 60);
+ 	 		System.out.println ("Even");
+	     }
+ 	 	else if (!(number % 2 ==0 && number > 60));
+	 		System.out.println (oddsandevens + " and Even");
+	 		
+	 	else if (!(number % 2 ==0 && number >= 60));
+ 	 		System.out.println (oddsandevens + " Odd and over 60");
+ 	 	}
+	else () {
+		system.out.println (oddsandevens + "and Odd")
+ 	 	   
+ 	 	}
+	
+	}
